056e9106928ca7b697a9bf46fb68a32191a99154
Yoni Fram <yonifram4@gmail.com>
2021-10-04 22:19:14 +0300

    Add project files.

---
+++ dotnet5782_2073_4574/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet5782_2073_4574
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
