98ab61c2b6506249a55758a7c3135467407da437
Yoni Fram <yonifram4@gmail.com>
2021-10-04 22:29:05 +0300

    Initial code + Welcome functions

---
+++ dotnet5782_2073_4574/Program4574.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4574();
+            Welcome2073();
+            Console.ReadKey();
+        }
+
+        private static void Welcome4574()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to our first(more like 200th) console application");
+        }
+        
+        static partial void Welcome2073()
+        {
+        }
+    }
+}
