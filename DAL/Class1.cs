372377a4a17a90d90fcd497eb258bb9147af3f47
Yoni Fram <yonifram4@gmail.com>
2021-10-11 12:16:25 +0300

    Add structs Drone and Package

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -1,5 +1,43 @@
 ﻿using System;
 
-namespace DAL
+namespace DAL.DO
 {
+    public enum WeightGroup { Light, Mid, Heavy }
+    public enum DroneStates { Light, Mid, Heavy }
+    public enum Priority { Low, Medium, High }
+
+    public struct Drone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public double Charge
+        {
+            get => Charge;
+            set
+            {
+                Charge = value < 100 && value > 0 ? value : Charge;
+            }
+        }
+        public WeightGroup Weight { get; set; }
+        public DroneStates State { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drone {Id}(Model num:{Model})";
+        }
+    }
+
+    public struct Package
+    {
+        public int Id {  get; set; }
+        public string SenderId { get; set; }
+        public string RecevirId { get; set; }
+        public WeightGroup Weight {  get; set; }
+        public Priority PackagePriority {  get; set; }
+        public int DroneId { get; set; }
+        public double TimeToPackage {  get; set; }
+        public double TimeToGetDrone { get; set; }
+        public double TimeToGetPackedge { get; set; }
+        public double TimeToRecive {  get; set; }
+    }
 }
