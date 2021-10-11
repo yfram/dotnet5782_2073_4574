7a3b89e290dae24a4f06a08ac94139d4b9ea2eb1
Yoni Fram <yonifram4@gmail.com>
2021-10-11 11:46:32 +0300

    Initialize ConsoleUI and DAL

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DAL;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
