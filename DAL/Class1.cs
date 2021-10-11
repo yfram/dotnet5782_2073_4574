f04fe02297088557f3f5ab7bf64f22ee0f31d720
Gil <gil.kovshi@gmail.com>
2021-10-11 12:08:26 +0300

    Create Station, Customer, DroneCharge structs.

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -2,4 +2,47 @@
 
 namespace DAL
 {
+    namespace DO
+    {
+        public struct Station
+        {
+            public int Id { get; set; }
+            public int Name { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+            public int ChargeSlots { get; set; }
+
+            public override string ToString()
+            {
+                return $"id: {Id}\n name: {Name}\n Longitude: {Longitude}\n Lattitude: {Lattitude}\n ChargeSlots: {ChargeSlots}";
+            }
+        }
+
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public int Name { get; set; }
+            public string Phone { get; set; }
+            public double Lattitude { get; set; }
+            public double Longitude { get; set; }
+
+            public override string ToString()
+            {
+                return $"Id: {Id}\n Name: {Name}\n Phone: {Phone}\nLongitude: {Longitude}\n Lattitude: {Lattitude}\n";
+            }
+        }
+
+        public struct DroneCharge
+        {
+            public int DroneId { get; set; }
+            public int StationId { get; set; }
+
+            public override string ToString()
+            {
+                return $"Drone id: {DroneId}\n Station id: {StationId}\n";
+            }
+
+
+        }
+    }
 }
