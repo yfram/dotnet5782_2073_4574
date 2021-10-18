321b7be8df06054ef78e844612fc3ca8bd4ab4b0
Yoni Fram <yonifram4@gmail.com>
2021-10-18 18:13:57 +0300

    Finish Intialize function

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -17,49 +17,44 @@ namespace DalObject
     }
     public class DataSource
     {
-        internal static Drone[] Drones = new Drone[10];
-        internal static DroneStates[] States = new DroneStates[5];
-        internal static Customer[] Customers = new Customer[100];
-        internal static Package[] Packages = new Package[1000];
 
         internal class Config
         {
-            internal static int DronesCounter = 0;
-            internal static int StatesCounter = 0;
-            internal static int CustomersCounter = 0;
-            internal static int PackagesCounter = 0;
+            internal static int FirstDrone = 0;
+            internal static int FirstStation = 0;
             internal static int RunNumber = 0;
-
         }
+
+        internal static Drone[] Drones = new Drone[10];
+        internal static Station[] Stations = new Station[5];
+        internal static Customer[] Customers = new Customer[100];
+        internal static Package[] Packages = new Package[1000];
+        internal static Config Configuration { get; set; }
 
         public static void Initialize()
         {
-            int droneNumber = 5;
-            for (int i = 0; i < droneNumber; i++)
-            {
-                Drones[i] = initDrone();
-            }
-
-
+            Configuration = new Config();  //Config is all 0's anyways
+            Random random = new Random();
+            for (int i = 0; i < 2; i++)
+                Stations[i] = InitStation(i, random);
+            for (int i = 0; i < 5; i++)
+                Drones[i] = InitDrone(i, random);
+            for (int i = 0; i < 10; i++)
+                Customers[i] = InitCustumer(i, random);
+            for (int i = 0; i < 10; i++)
+                Packages[i] = InitPackage(i, random);
         }
 
-        /*
-         * needs fixing to actually work
-          private static GetEnumer(Enum myEnum)
-        {
-            Array arr = Enum.GetValues(myEnum.GetType());
-            Random r = new Random();
-            return (myEnum.GetType())arr.GetValue(r.Next(0, arr.Length));
-        }*/
-        private static Drone initDrone()
-        {
-            return new Drone(Config.DronesCounter++, "TEST", 1, GetEnumer(WeightGroup), GetEnumer(DroneStates));
-        }
+        private static Package InitPackage(int i, Random random) => new(i, Customers[i].Id, Customers[(i + 11) % 10].Id, (WeightGroup)random.Next(), (Priority)(i % 3),
+            0, 0, 0, 0, 0);
+        private static Station InitStation(int i, Random random) => new(i, $"Station {i}", 0, 0, random.Next() % Station.MaxChargingPorts);
+        private static Drone InitDrone(int i, Random random) => new(i, "TBD", random.NextDouble(), (WeightGroup)(random.Next()), (DroneStates)(i % 3));
+        private static Customer InitCustumer(int i, Random random) => new(i, $"Person #{i}", GeneratePhone(), random.NextDouble(), random.NextDouble());
 
-        private static Customer initCustumer()
+        private static string GeneratePhone()
         {
-            return new Customer();
+            Random random = new Random();
+            return $"+972-5{random.Next() % 10}{random.Next(1000000, 9999999)}";
         }
     }
-}
 }
