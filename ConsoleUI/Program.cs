1432a1f240c994bdf70133d38c7ee7ad6d6ca568
Gil <gil.kovshi@gmail.com>
2021-10-19 12:55:34 +0300

    finish GetListMenu and GetByIdMenu functions

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,33 +8,19 @@ namespace ConsoleUI
     class Program
     {
         enum Menus { Add = 1, Update, GetById, GetList, Exit }
+
+        
         static void Main(string[] args)
         {
+            DalObject.DalObject d = new DalObject.DalObject();
+
             String openMsg = "Welcome to John&G Drones administiration system!\n" +
                 "for add menu, press 1\n" +
                 "for update menu, press 2\n" +
                 "for get by id menu, press 3\n" +
                 "for get lists menu, press 4\n" +
                 "to exit press 5\n";
-            String addMenu = "to add a base station, press 1\n" +
-                "to add a drone, press 2\n" +
-                "to add a new customer, press 3\n" +
-                "to add a new packeage for delivering, press 4\n";
-            String updateMenu = "to associate a package to a drone, press 1\n" +
-                "to pick up a package by a drone, press 2\n" +
-                "to deliver a package to a customer, press 3\n" +
-                "to send a drone to charge, press 4\n" +
-                "to release a drone from a station, press 5\n";
-            String getByIdMenu = "to get a station by id press 1\n" +
-                "to get a drone by id press 2\n" +
-                "to get a customer by id press 3\n" +
-                "to get a package by id press 4\n";
-            String getListsMenu = "to get the station list press 1\n" +
-                "to get the drones list press 2\n" +
-                "to get the customers list press 3\n" +
-                "to get the packages list press 4\n" +
-                "to get a list of all packages wihtout drones press 5\n" +
-                "to get a list of all the available stations press 6\n";
+
 
             int menu;
             do
@@ -46,16 +32,16 @@ namespace ConsoleUI
             switch ((Menus)menu)
             {
                 case Menus.Add:
-                    AddMenu();
+                    AddMenu(d);
                     break;
                 case Menus.Update:
-                    UpdateMenu();
+                    UpdateMenu(d);
                     break;
                 case Menus.GetById:
-                    GetByIdMenu();
+                    GetByIdMenu(d);
                     break;
                 case Menus.GetList:
-                    GetListMenu();
+                    GetListMenu(d);
                     break;
                 case Menus.Exit:
                     Console.WriteLine("GoodBye!");
@@ -64,24 +50,136 @@ namespace ConsoleUI
             }
         }
 
-        private static void GetListMenu()
+        private static void GetListMenu(DalObject.DalObject d)
         {
-            throw new NotImplementedException();
+            String getListsMenu = "to get the station list press 1\n" +
+                "to get the drones list press 2\n" +
+                "to get the customers list press 3\n" +
+                "to get the packages list press 4\n" +
+                "to get a list of all packages wihtout drones press 5\n" +
+                "to get a list of all the available stations press 6\n";
+
+            Console.Write(getListsMenu);
+
+            int menu;
+            do
+            {
+                Console.Write(getListsMenu);
+                menu = Console.Read();
+            } while (!(menu <= 6 && menu >= 1));
+
+            String str = "";
+            switch (menu)
+            {
+                case 1:
+                    str = d.GetAllStationsString();
+                    break;
+                case 2:
+                    str = d.GetAllDronesString();
+                    break;
+                case 3:
+                    str = d.GetAllCustomersString();
+                    break;
+                case 4:
+                    str = d.GetAllPackagesString();
+                    break;
+                case 5:
+                    str = d.GetAllUndronedPackagesString();
+                    break;
+                case 6:
+                    str = d.GetAllAvailableStationsString();
+                    break;
+            }
+
+            Console.WriteLine(str);
+
         }
 
-        private static void GetByIdMenu()
+        private static void GetByIdMenu(DalObject.DalObject d)
         {
-            throw new NotImplementedException();
+            String getByIdMenu = "to get a station by id press 1\n" +
+                "to get a drone by id press 2\n" +
+                "to get a customer by id press 3\n" +
+                "to get a package by id press 4\n";
+
+            Console.Write(getByIdMenu);
+
+            int menu;
+            do
+            {
+                Console.Write(getByIdMenu);
+                menu = Console.Read();
+            } while (!(menu <= 4 && menu >= 1));
+
+            Console.Write("enter ID:\n");
+            int id = Console.Read();
+
+            String str = "";
+            switch (menu)
+            {
+                case 1: 
+                    str = d.GetStationString(id);
+                    break;
+                case 2:
+                    str = d.GetDroneString(id);
+                    break;
+                case 3:
+                    str = d.GetCustomerString(id);
+                    break;
+                case 4:
+                    str = d.GetPackageString(id);
+                    break;
+            }
+
+            Console.WriteLine(str);
         }
 
-        private static void UpdateMenu()
+        private static void UpdateMenu(DalObject.DalObject d)
         {
-            throw new NotImplementedException();
+            String updateMenu = "to associate a package to a drone, press 1\n" +
+                "to pick up a package by a drone, press 2\n" +
+                "to deliver a package to a customer, press 3\n" +
+                "to send a drone to charge, press 4\n" +
+                "to release a drone from a station, press 5\n";
+
+            Console.Write(updateMenu);
+
+            int menu;
+            do
+            {
+                Console.Write(updateMenu);
+                menu = Console.Read();
+            } while (!(menu <= 5 && menu >= 1));
+
+
+
         }
 
-        private static void AddMenu()
+        private static void AddMenu(DalObject.DalObject d)
         {
-            throw new NotImplementedException();
+            String addMenu = "to add a base station, press 1\n" +
+                "to add a drone, press 2\n" +
+                "to add a new customer, press 3\n" +
+                "to add a new packeage for delivering, press 4\n";
+
+            Console.Write(addMenu);
+
+            int menu;
+            do
+            {
+                Console.Write(addMenu);
+                menu = Console.Read();
+            } while (!(menu <= 4 && menu >= 1));
+
+            switch(menu)
+            {
+                case 1:
+                    DalObject.DalObject.AddStation(Console.Read(), Console.ReadLine(), Double.TryParse(Console.ReadLine()), );
+                case 2:
+                case 3:
+                case 4:
+            }
+
         }
     }
 }
