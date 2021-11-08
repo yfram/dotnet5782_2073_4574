1ec36b9738f7ee5a5b1d48bb32c2997666ec828d
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:33:18 +0200

    Create DroneForPackage

---
+++ BL/DroneForPackage.cs
@@ -0,0 +1,9 @@
+﻿namespace IBL.BO
+{
+    public class DroneForPackage
+    {
+        public int Id { get; set; }
+        public double Battery { get; set; }
+        public Location CurrentLocation { get; set; }
+    }
+}
