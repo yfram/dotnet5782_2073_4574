943c907c5ce57ac864ef9acf67ef185cc41d298e
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:44:25 +0200

    Create DroneForList

---
+++ BL/DroneForList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class DroneForList
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightGroup Weight { get; set; }
+        public double Battery { get; set; }
+        public DroneState State { get; set; }
+        public Location CurrentLocation { get; set; }
+        public int? PassingPckageId { get; set; } = null;
+    }
+}
