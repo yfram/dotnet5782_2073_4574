1f13bc8f8756bf326b26dd958b9acfe758f8eda4
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:40:03 +0200

    Create Drone

---
+++ BL/Drone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class Drone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightGroup Weight { get; set; }
+        public double Battery { get; set; }
+        public DroneState State { get; set; }
+        public PackageInTransfer Package { get; set; }
+        public Location CurrentLocation { get; set; }
+    }
+}
