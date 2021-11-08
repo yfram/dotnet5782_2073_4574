cbd7bca73dafeb86c466368129a24f5922f71da7
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:45:29 +0200

    Create DroneInCharging

---
+++ BL/DroneInCharging.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class DroneInCharging
+    {
+        public int Id { get; set; }
+        public double Battery { get; set; }
+    }
+}
