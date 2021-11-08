229a2e19911dbba2ddd653f73aedeaaf8ebfe039
Yoni Fram <yonifram4@gmail.com>
2021-11-08 11:45:43 +0200

    Create Customer

---
+++ BL/Customer.cs
@@ -0,0 +1,20 @@
+﻿
+using IDAL.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class Customer
+    {
+        public int Id {  get; set; }
+        public string Name {  get; set; }
+        public string PhoneNumber { get; set; }
+        public Location CustomerLocation {  get; set; }
+        public List<Package> PackagesFrom {  get; set; }
+        public List<Package> PackagesTo {  get; set; }
+    }
+}
