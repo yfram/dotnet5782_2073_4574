d3c8063530b5dfac5707a1055f31727e579d2b5b
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:49:43 +0200

    Create StationForList

---
+++ BL/StationForList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class StationForList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AmountOfEmptyPorts { get; set; }
+        public int AmountOfFullPorts { get; set; }
+    }
+}
