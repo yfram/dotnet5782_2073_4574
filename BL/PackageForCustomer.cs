826e0121ce8a1b82cf33c96c9418c06cd1ce3c98
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:22:25 +0200

    Create PackageForCustomer

---
+++ BL/PackageForCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class PackageForCustomer
+    {
+        public int Id { get; set; }
+        public WeightGroup Weight { get; set; }
+        public PriorityGroup Priority { get; set; }
+        public PackageStatus Status { get; set; }
+        public CustomerForPackage Customer { get; set; }
+    }
+}
