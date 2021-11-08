84e656564457a781870f4488c4df2f66bb180723
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:48:04 +0200

    Create Station

---
+++ BL/Station.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Location LocationOfStation { get; set; }
+        public int AmountOfEmptyPorts { get; set; }
+        public List<DroneInCharging> ChargingDrones { get; set; } = new();
+    }
+}
