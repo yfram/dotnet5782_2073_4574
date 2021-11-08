8ceb8349e5fe45e2a16d8d83685d061825fd8073
Yoni Fram <yonifram4@gmail.com>
2021-11-08 12:02:27 +0200

    Create Enums

---
+++ BL/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public enum WeightGroup
+    {
+        Light, Mid, Heavy
+    }
+    public enum Priority
+    {
+        Normal, Fast, Urgent
+    }
+}
