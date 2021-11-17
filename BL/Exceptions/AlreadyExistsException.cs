e59524e000eb06cebc892c4d6162cb63a120dd1f
Yoni Fram <yonifram4@gmail.com>
2021-11-17 21:14:29 +0200

    Create AlreadyExistsException exception

---
+++ BL/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,32 @@
+﻿using IBL.BO;
+using System;
+using System.Runtime.Serialization;
+
+namespace IBL
+{
+    internal class AlreadyExistsException : Exception
+    {
+        private int id;
+
+        public AlreadyExistsException()
+        {
+        }
+
+        public AlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public AlreadyExistsException(string message, int _id) : this(message)
+        {
+            id = _id;
+        }
+
+        public AlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
