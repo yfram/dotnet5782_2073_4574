08cb10ad50e2917da9f74990161f2bdafb243bd5
Yoni Fram <yonifram4@gmail.com>
2021-11-18 12:34:03 +0200

    Rename DoesNotExistException

---
+++ BL/Exceptions/DoesNotExistsException.cs
@@ -0,0 +1,32 @@
+﻿using IBL.BO;
+using System;
+using System.Runtime.Serialization;
+
+namespace IBL
+{
+    internal class DoesNotExistsException : Exception
+    {
+        private int id;
+
+        public DoesNotExistsException()
+        {
+        }
+
+        public DoesNotExistsException(string message) : base(message)
+        {
+        }
+
+        public DoesNotExistsException(string message, int _id) : this(message)
+        {
+            id = _id;
+        }
+
+        public DoesNotExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DoesNotExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
