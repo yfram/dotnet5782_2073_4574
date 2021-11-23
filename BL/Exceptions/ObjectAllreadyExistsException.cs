f393e2b876328d87b63094436783ff4f628237a5
Yoni Fram <yonifram4@gmail.com>
2021-11-23 12:22:16 +0200

    Create ObjectDoesntExistException

---
+++ BL/Exceptions/ObjectAllreadyExistsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.Exceptions
+{
+    public class ObjectDoesntExistException : Exception
+    {
+        public ObjectDoesntExistException()
+        {
+        }
+
+        public ObjectDoesntExistException(string message) : base(message)
+        {
+        }
+
+        public ObjectDoesntExistException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ObjectDoesntExistException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
