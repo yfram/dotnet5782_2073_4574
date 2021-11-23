743db85d01504bc77e73a9c50d49c43bf2c257b6
Yoni Fram <yonifram4@gmail.com>
2021-11-23 12:06:21 +0200

    Create BlException

---
+++ BL/Exceptions/BlException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.Exceptions
+{
+    public class BlException : Exception
+    {
+        internal int ObjectId;
+        internal Type TypeOfWrongObject;
+
+        public BlException(string message, int id, Type typeOfWrongObject) : base(message)
+        {
+            ObjectId = id;
+            TypeOfWrongObject = typeOfWrongObject;
+        }
+
+        public BlException()
+        {
+        }
+
+        public BlException(string message) : base(message)
+        {
+        }
+
+        public BlException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BlException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
