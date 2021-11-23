f2008fa9d932f28f166bb865950de37fd82ff7f4
Gil <gil.kovshi@gmail.com>
2021-11-23 22:31:06 +0200

    throw exceptions in BL

---
+++ BL/Exceptions/DroneStateException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.Exceptions
+{
+    public class DroneStateException : Exception
+    {
+        public DroneStateException()
+        {
+        }
+
+        public DroneStateException(string message) : base(message)
+        {
+        }
+
+        public DroneStateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DroneStateException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
