fe1703cbbc4c34115af7aff9cb3d810de835aee2
Gil <gil.kovshi@gmail.com>
2021-12-06 18:05:13 +0200

    add location to string

--- BL/Data types/Location.cs
+++ BL/Data types/Location.cs
@@ -1,4 +1,6 @@
-﻿namespace IBL.BO
+﻿using CoordinateSharp;
+
+namespace IBL.BO
 {
     public class Location
     {
@@ -6,11 +8,18 @@
         {
             Longitude = longitude;
             Latitude = latitude;
+            
         }
 
         public double Longitude { get; set; }
         public double Latitude { get; set; }
 
-        public override string ToString() => $"Longitude: {Longitude}\nLatitude: {Latitude}";
+        public override string ToString()
+        {
+            Coordinate c = new Coordinate(Latitude,Longitude);// $"Longitude: {Longitude}\nLatitude: {Latitude}";
+            c.FormatOptions.Format = CoordinateFormatType.Degree_Minutes_Seconds;
+            return c.ToString();
+        }
+
     }
 }
