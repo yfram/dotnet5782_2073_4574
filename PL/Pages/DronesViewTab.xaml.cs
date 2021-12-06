a1ed8e3c8ba04af16c7b1b479d6592af2e87a5e1
Yoni Fram <yonifram4@gmail.com>
2021-12-06 17:35:29 +0200

    Create drones view

--- PL/Pages/DronesViewTab.xaml.cs
+++ PL/Pages/DronesViewTab.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using IBL.BO;
 
 namespace PL.Pages
 {
@@ -20,8 +11,10 @@ namespace PL.Pages
     /// </summary>
     public partial class DronesViewTab : UserControl
     {
+        public List<Drone> Drones { get; set; } = new();
         public DronesViewTab()
         {
+            MainWindow.BL.DisplayDrones().ToList().ForEach(d => Drones.Add(MainWindow.BL.DisplayDrone(d.Id)));
             InitializeComponent();
         }
     }
