1a2408a5eab88df2093c83dae99de1326a723649
Yoni Fram <yonifram4@gmail.com>
2021-12-11 23:03:42 +0200

    Create DalFactory

---
+++ DAL/DalApi/DalFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public static class DalFactory
+    {
+        public static IDAL GetDal(string param)
+        {
+            return param switch
+            {
+                "DalObject" => new DalObject.DalObject(),
+                "DalXml" => new DalXml(),
+                _ => throw new ArgumentException($"{param} is not a recognized class or interface")
+            };
+        }
+    }
+}
