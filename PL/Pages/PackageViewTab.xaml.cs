f491e9208c69357e60be6d907d5c45abb84efc98
Yoni Fram <yonifram4@gmail.com>
2021-12-21 12:33:11 +0200

    Add basic package view

---
+++ PL/Pages/PackageViewTab.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BO;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PL.Pages
+{
+    /// <summary>
+    /// Interaction logic for DroneViewTab.xaml
+    /// </summary>
+    public partial class PackageViewTab : UserControl
+    {
+        private Package BLpackage { get => Resources["package"] as Package; set => Resources["package"] = value; }
+
+        public PackageViewTab(int id)
+        {
+            InitializeComponent();
+            BLpackage = MainWindow.BL.DisplayPackage(id);
+        }
+    }
+}
