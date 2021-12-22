0562511468e7e044454beebaabaa662d7d8c2ef9
Gil <gil.kovshi@gmail.com>
2021-12-22 17:23:19 +0200

    converter

--- PL/Pages/DronesConvertor.cs
+++ PL/Pages/DronesConvertor.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace PL.Pages
@@ -12,7 +15,20 @@ namespace PL.Pages
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return 0;
+            if (value is not ObservableCollection<Drone> drones) return null;
+            /*
+            bool? boxChecked = ((CheckBox)parameter).IsChecked;
+            bool isCollected = boxChecked.HasValue ? boxChecked.Value : false;
+            */
+            bool isCollected = true;
+            if (isCollected)
+            {
+                return drones.GroupBy(d => d.State);
+            }
+            else
+            {
+                return drones;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
