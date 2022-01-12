55b1ca5cc85bef26c329360147ac1c90a07a3824
Yoni Fram <yonifram4@gmail.com>
2022-01-12 16:49:21 +0200

    Add multiple theme support

--- PL/App.xaml.cs
+++ PL/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿// File {filename} created by Yoni Fram and Gil Kovshi
 // All rights reserved
 
+using System;
+using System.Collections.Generic;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace PL
 {
@@ -10,5 +13,25 @@ namespace PL
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ChangeTheme("");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
+            key = key.CreateSubKey("Drones Manager", true);
+            key = key.CreateSubKey($"Version {}");
+            base.OnExit(e);
+        }
+
+        private void ChangeTheme(string themePath)
+        {
+            Resources.MergedDictionaries.RemoveAt(Resources.MergedDictionaries.Count - 1);
+            Resources.MergedDictionaries.Add(new ResourceDictionary()
+            { Source = new Uri($@"/Assets/Themes/{themePath}Theme.xaml", UriKind.Relative) });
+        }
     }
 }
