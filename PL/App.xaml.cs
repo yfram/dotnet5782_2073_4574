5537ea33db7bb2acd17cae6d3d31d37306b2cbe7
Yoni Fram <yonifram4@gmail.com>
2022-01-12 17:30:50 +0200

    Save theme for next run

--- PL/App.xaml.cs
+++ PL/App.xaml.cs
@@ -2,9 +2,9 @@
 // All rights reserved
 
 using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using Microsoft.Win32;
 
 namespace PL
 {
@@ -13,23 +13,28 @@ namespace PL
     /// </summary>
     public partial class App : Application
     {
+        private string currentTheme = "Dark";
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            ChangeTheme("");
+            if (File.Exists(".userSettings"))
+                currentTheme = File.ReadAllLines(".usersettings").
+                    Where(l => l.StartsWith("default_theme="))
+                    .First().Replace("default_theme=", "");
+            ChangeTheme(currentTheme);
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
-            RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true);
-            key = key.CreateSubKey("Drones Manager", true);
-            key = key.CreateSubKey($"Version {}");
+            if (currentTheme != "Default")
+                File.WriteAllText(".usersettings", $"default_theme={currentTheme}");
             base.OnExit(e);
         }
 
         private void ChangeTheme(string themePath)
         {
-            Resources.MergedDictionaries.RemoveAt(Resources.MergedDictionaries.Count - 1);
+            currentTheme = themePath;
             Resources.MergedDictionaries.Add(new ResourceDictionary()
             { Source = new Uri($@"/Assets/Themes/{themePath}Theme.xaml", UriKind.Relative) });
         }
